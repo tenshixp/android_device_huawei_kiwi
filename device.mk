09eda852729f57320f8b0e69c7614e59d14b1b9a
tenshixp <danielc1_1@yahoo.com.mx>
2018-03-07 00:31:19 -0500

    kiwi: Add FEATURE_OPENGLES_EXTENSION_PACK support
    This change exposes the support for Android extension pack (AEP) in the
    graphics driver by copying the android.hardware.opengles.aep.xml to the
    /system/etc/permissions directory on the device
    
    Change-Id: Idac9f2c77590664c8076c7b25b1ee9afe022c616

--- device.mk
+++ device.mk
@@ -43,6 +43,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardware.sensor.gyroscope.xml \
     frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
     frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
+    frameworks/native/data/etc/android.hardware.opengles.aep.xml:system/etc/permissions/android.hardware.opengles.aep.xml \
     frameworks/native/data/etc/android.software.app_widgets.xml:system/etc/permissions/android.software.app_widgets.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml
 
